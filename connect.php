528da133bc46fe4f12a4563ac63900666f8f1f49
Sophie <sophbyr@gmail.com>
2019-05-13 22:26:46 +0200

    [+]Backend : Update connect and show project

--- connect.php
+++ connect.php
@@ -1,6 +1,12 @@
 <?php
 try {
-    $connexion = new PDO('mysql:host=localhost;dbname=id9153549_goudimac;charset=utf8', 'id9153549_admin', 'ImacKickstarter02');
+	require_once 'MyPDO.class.php';
+	MyPDO::setConfiguration('mysql:host=localhost;dbname=id9153549_goudimac;charset=utf8', 'id9153549_admin', 'ImacKickstarter02');
+
+	// ID Sophie ne pas effacer
+	PDO::setConfiguration('mysql:host=localhost;dbname=id9153549_goudimac;charset=utf8', 'root', ''); 
+   //$connexion = new PDO('mysql:host=localhost;dbname=id9153549_goudimac;charset=utf8', 'id9153549_admin', 'ImacKickstarter02');
+
 } catch (PDOException $e) {
     print "Erreur !: " . $e->getMessage() . "<br/>";
     die();
