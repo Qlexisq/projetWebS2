fad3ca9dbe9aefafa634819c414cfabb58094db8
Sophie <sophbyr@gmail.com>
2019-05-18 13:18:46 +0200

    [+]Backend : fix insert php

--- php/insert-register.php
+++ php/insert-register.php
@@ -10,17 +10,17 @@ if(isset($_GET['name-register']) && isset($_GET['firstname-register']) && isset(
 {
     // verification à faire
     echo json_encode("find request");
-    $name = $_GET['name-register']
+    $name = $_GET['name-register'];
     $firstname = $_GET['firstname-register'];
     $pseudo = $_GET['pseudo-register'];
     $mail = $_GET['mail-register'];
     $password = $_GET['password-register'];
     $stmt = MyPDO::getInstance()->prepare(<<<SQL
-    INSERT INTO user (firstname, lastname, pseudo, mail, codeword)
-    VALUES(:firstname, :lastname, :pseudo, :mail, :codeword);
+    INSERT INTO user (firstname, lastname, pseudo, mail, password)
+    VALUES(:firstname, :lastname, :pseudo, :mail, :password);
 SQL
 );
-    $stmt->execute(['firstname' => $firstname, 'lastname' => $lastname, 'pseudo' => $pseudo, 'mail' => $pseudo, 'codeword' => $password]);
+    $stmt->execute(['firstname' => $firstname, 'lastname' => $name, 'pseudo' => $pseudo, 'mail' => $pseudo, 'password' => $password]);
 }else 
 {
     http_response_code(404);
