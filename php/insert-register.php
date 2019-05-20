2cc94c40d22db7e7a44cca78183bdd3aff04967d
Unknown <rvallee@etud.u-pem.fr>
2019-05-20 09:41:47 +0200

    update insert-register.php

--- php/insert-register.php
+++ php/insert-register.php
@@ -16,22 +16,19 @@ if(isset($_GET['name-register']) && isset($_GET['firstname-register']) && isset(
     $pseudo = htmlspecialchars($_GET['pseudo-register']);
     $mail = htmlspecialchars($_GET['mail-register']);
     $password = htmlspecialchars($_GET['password-register']);
-    $verif = MyPDO::getInstance()->prepare(<<<SQL
-   SELECT mail, pseudo FROM user;
+
+    //verification pseudo
+    $stmt = MyPDO::getInstance()->prepare(<<<SQL
+   SELECT pseudo FROM user;
 SQL
 );
-    $verif->execute();
+    $stmt->execute();
+    $stmt->setFecthMode(PDO::FETCH_ASSOC);
     while (($row = $stmt->fetch()) !== false) {
         array_push($projects, $row);
     }
     foreach($row as $value)
     {
-        if($value == $mail)
-        {
-            http_response_code(404);
-            echo json_encode("erreur form register");
-            exit();
-        }
         if($value == $pseudo)
         {
             http_response_code(404);
@@ -39,6 +36,26 @@ SQL
             exit();
         }
     }
+
+    //verification mail
+    $stmt = MyPDO::getInstance()->prepare(<<<SQL
+    SELECT mail FROM user;
+SQL
+);
+     $stmt->execute();
+     $stmt->setFecthMode(PDO::FETCH_ASSOC);
+     while (($row = $stmt->fetch()) !== false) {
+         array_push($projects, $row);
+     }
+     foreach($row as $value)
+     {
+         if($value == $mail)
+         {
+             http_response_code(404);
+             echo json_encode("erreur form register");
+             exit();
+         }
+     }
 
     $stmt = MyPDO::getInstance()->prepare(<<<SQL
     INSERT INTO user (firstname, lastname, pseudo, mail, password)
